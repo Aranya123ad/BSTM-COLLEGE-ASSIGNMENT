b7e4f56b5b70abbb18e5634a9c722040e54aa90f
Aranya123ad <110688581+Aranya123ad@users.noreply.github.com>
2023-04-29 23:14:03 +0530

    Update wap to print name and college name.py

--- wap to print name and college name.py
+++ wap to print name and college name.py
@@ -1,2 +1,10 @@
 print("My name is Aranya Biswas")
 print("My college name is Bengal School Of Technology And Management")
+
+
+
+
+
+OUTPUT:-
+My name is Aranya Biswas
+My college name is Bengal School Of Technology And Management
